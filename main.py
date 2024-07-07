b86de8378550ccf5bb164ca7b74dad0837bd075f
Leander <lflopez@ualberta.ca>
2024-07-07 15:43:08 -0600

    refactored functions into a class, fixed excesive download problem

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 from skyfield.api import load, wgs84
 from satallitefunctions import *
 
+MainTleReader = TleReader()
+
 # Get the epoch
-print_epoch()
+MainTleReader.print_epoch()
+print(" ")
 
 # Get overhead times --------------------------------------------------------------------------------
 
 ts = load.timescale()
-t0 = ts.utc(2024, 6, 25)
+t0 = ts.utc(2024, 7, 1)
 t1 = ts.utc(2024, 7, 30)
-print_overhead_times(t0, t1, 63.9)
+MainTleReader.print_overhead_times(t0, t1, 63)
+print(" ")
 
 # Get sunlit times -----------------------------------------------------------------------------------
-utc_timerange = ts.utc(2024, 7, 5, 0, range(0, 24 * 60, 1))
-#print_sunlit_ranges(utc_timerange)
+utc_timerange = ts.utc(2024, 7, 7, 0, range(0, 24 * 60, 1))
+MainTleReader.print_sunlit_ranges(utc_timerange)
+print(" ")
 
 # Get in ecclipse time -------------------------------------------------------------------------------
-print_ecclipse_ranges(utc_timerange)
+MainTleReader.print_ecclipse_ranges(utc_timerange)
 
--- satallitefunctions.py
+++ satallitefunctions.py
@@ -1,96 +1,102 @@
 from skyfield.api import load, wgs84
+from skyfield.iokit import parse_tle_file
 
-stations_url = 'https://celestrak.org/NORAD/elements/gp.php?INTDES=1998-067&FORMAT=tle'
+class TleReader:
+    def __init__(self):
+        self.stations_url = 'https://celestrak.org/NORAD/elements/gp.php?INTDES=1998-067&FORMAT=tle'
+        self.satellite = None
 
-def print_epoch():
-    satellites = load.tle_file(stations_url, reload=True)
-    by_name = {sat.name: sat for sat in satellites}
-    satellite = by_name['ISS (ZARYA)']
+        self.access_TLE()
 
-    epoch_object = satellite.epoch.utc_jpl()
-    print(epoch_object)
+    # This method must be called before any method, however it only needs to be called once
+    def access_TLE(self):
+        max_days = 7 
+        name = 'gp.php'
 
-def print_overhead_times(t0, t1, cutoff_angle):
-    satellites = load.tle_file(stations_url, reload=True)
-    by_name = {sat.name: sat for sat in satellites}
-    satellite = by_name['ISS (ZARYA)']
+        # Download again if it's been a while
+        if not load._exists(name) or load.days_old(name) >= max_days:
+            load.download(self.stations_url, filename=name)
+            print("New TLE Downloaded")
+        
+        # Set self.satelite so that other methods will now work
+        ts = load.timescale()
+        with load.open('gp.php') as f:
+            satellites = list(parse_tle_file(f, ts))
+            by_name = {sat.name: sat for sat in satellites}
+            self.satellite = by_name['ISS (ZARYA)']
 
-    bluffton = wgs84.latlon(+53.527413, -113.530055)
+    def print_epoch(self):
+        epoch_object = self.satellite.epoch.utc_jpl()
+        print(epoch_object)
 
-    t, events = satellite.find_events(bluffton, t0, t1, altitude_degrees=cutoff_angle)
-    event_names = 'rise above ' + str(cutoff_angle) + ' °', 'culminate', 'set below ' + str(cutoff_angle) + ' °'
-    print(" ")
-    for ti, event in zip(t, events):
-        name = event_names[event]
-        print(ti.utc_strftime('%Y %b %d %H:%M:%S'), name)
+    def print_overhead_times(self, t0, t1, cutoff_angle):
+        bluffton = wgs84.latlon(+53.527413, -113.530055)
 
-def print_sunlit_ranges(utc_timerange):
-    satellites = load.tle_file(stations_url, reload=True)
-    by_name = {sat.name: sat for sat in satellites}
-    satellite = by_name['ISS (ZARYA)']
+        t, events = self.satellite.find_events(bluffton, t0, t1, altitude_degrees=cutoff_angle)
+        event_names = 'rise above ' + str(cutoff_angle) + ' °', 'culminate', 'set below ' + str(cutoff_angle) + ' °'
 
-    ts = load.timescale()
-    eph = load('de421.bsp')
+        for ti, event in zip(t, events):
+            name = event_names[event]
+            print(ti.utc_strftime('%Y %b %d %H:%M:%S'), name)
 
-    sunlit = satellite.at(utc_timerange).is_sunlit(eph)
+    def print_sunlit_ranges(self, utc_timerange):
+        eph = load('de421.bsp')
 
-    sunlit_beginnings = []
-    sunlit_endings = []
+        sunlit = self.satellite.at(utc_timerange).is_sunlit(eph)
 
-    in_sunlit_period = False
-    for ti, sunlit_i in zip(utc_timerange, sunlit):
-        if sunlit_i:
-            if not in_sunlit_period:
-                sunlit_beginnings.append(ti)
-            in_sunlit_period = True
-        else:
-            if in_sunlit_period:
-                sunlit_endings.append(ti)
-            in_sunlit_period = False
+        sunlit_beginnings = []
+        sunlit_endings = []
 
-    # account for edge case where a sunlit period begins during the time window but ends after
-    if len(sunlit_beginnings) > len(sunlit_endings):
-        sunlit_endings.append(utc_timerange[-1])
-    # this edge case only exists on the end, because we assume not sunlit before the beginning of the time window
+        in_sunlit_period = False
+        for ti, sunlit_i in zip(utc_timerange, sunlit):
+            if sunlit_i:
+                if not in_sunlit_period:
+                    sunlit_beginnings.append(ti)
+                in_sunlit_period = True
+            else:
+                if in_sunlit_period:
+                    sunlit_endings.append(ti)
+                in_sunlit_period = False
 
-    for start, end in zip(sunlit_beginnings, sunlit_endings):
-        print('{} - {}'.format(
-            start.utc_strftime('%Y-%m-%d %H:%M'),
-            end.utc_strftime('%Y-%m-%d %H:%M')
-        ))
+        # account for edge case where a sunlit period begins during the time window but ends after
+        if len(sunlit_beginnings) > len(sunlit_endings):
+            sunlit_endings.append(utc_timerange[-1])
+        # this edge case only exists on the end, because we assume not sunlit before the beginning of the time window
 
-def print_ecclipse_ranges(utc_timerange):
-    satellites = load.tle_file(stations_url, reload=True)
-    by_name = {sat.name: sat for sat in satellites}
-    satellite = by_name['ISS (ZARYA)']
+        for start, end in zip(sunlit_beginnings, sunlit_endings):
+            print('{} - {}'.format(
+                start.utc_strftime('%Y-%m-%d %H:%M'),
+                end.utc_strftime('%Y-%m-%d %H:%M')
+            ))
 
-    eph = load('de421.bsp')
+    def print_ecclipse_ranges(self, utc_timerange):
+        eph = load('de421.bsp')
 
-    earth, sun = eph['earth'], eph['sun']
-    p = (earth + satellite).at(utc_timerange).observe(sun).apparent()
-    eclipse = p.is_behind_earth()
+        earth, sun = eph['earth'], eph['sun']
+        p = (earth + self.satellite).at(utc_timerange).observe(sun).apparent()
+        eclipse = p.is_behind_earth()
 
-    eclipse_beginnings = []
-    eclipse_endings = []
+        eclipse_beginnings = []
+        eclipse_endings = []
 
-    in_eclipse_period = False
-    for ti, eclipse_i in zip(utc_timerange, eclipse):
-        if eclipse_i:
-            if not in_sunlit_period:
-                eclipse_beginnings.append(ti)
-            in_sunlit_period = True
-        else:
-            if in_sunlit_period:
-                eclipse_endings.append(ti)
-            in_sunlit_period = False
+        in_eclipse_period = False
+        for ti, eclipse_i in zip(utc_timerange, eclipse):
+            if eclipse_i:
+                if not in_eclipse_period:
+                    eclipse_beginnings.append(ti)
+                in_eclipse_period = True
+            else:
+                if in_eclipse_period:
+                    eclipse_endings.append(ti)
+                in_eclipse_period = False
 
-    # account for edge case where a sunlit period begins during the time window but ends after
-    if len(eclipse_beginnings) > len(eclipse_endings):
-        eclipse_endings.append(utc_timerange[-1])
-    # this edge case only exists on the end, because we assume not sunlit before the beginning of the time window
+        # account for edge case where a sunlit period begins during the time window but ends after
+        if len(eclipse_beginnings) > len(eclipse_endings):
+            eclipse_endings.append(utc_timerange[-1])
+        # this edge case only exists on the end, because we assume not sunlit before the beginning of the time window
 
-    for start, end in zip(eclipse_beginnings, eclipse_endings):
-        print('{} - {}'.format(
-            start.utc_strftime('%Y-%m-%d %H:%M'),
-            end.utc_strftime('%Y-%m-%d %H:%M')
-        ))
+        for start, end in zip(eclipse_beginnings, eclipse_endings):
+            print('{} - {}'.format(
+                start.utc_strftime('%Y-%m-%d %H:%M'),
+                end.utc_strftime('%Y-%m-%d %H:%M')
+            ))
